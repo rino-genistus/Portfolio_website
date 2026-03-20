f8904892a948cb5724b3e00f8fd1af9b96bd592c
Rino Genistus Ruban <Rino.genistus@gmail.com>
2026-03-20 01:46:52 -0400

    Fixed Mobile rendering of Back to top button, and lag with background glow

--- rino-portfolio/src/CursorGlow.jsx
+++ rino-portfolio/src/CursorGlow.jsx
@@ -1,18 +1,26 @@
-import {useEffect, useState} from 'react'
+import {useEffect, useRef} from 'react'
 
 function CursorGlow(){
-    const [pos, setPos] = useState({x:-200, y:-200})
+    const glowRef = useRef(null)
+    const rafRef = useRef(null)
 
     useEffect(() => {
         const handleMouseMove = (e) => {
-            setPos({x:e.clientX, y:e.clientY})
-        }
-        window.addEventListener('mousemove', handleMouseMove, {passive:true})
-        return () => window.removeEventListener('mousemove', handleMouseMove)
+            if (rafRef.current) cancelAnimationFrame(rafRef.current)
+                rafRef.current = requestAnimationFrame(() => {
+            glowRef.current.style.left = `${e.clientX}px`
+            glowRef.current.style.top  = `${e.clientY}px`
+      })
+    }
+    window.addEventListener('mousemove', handleMouseMove, {passive:true})
+    return () => {
+        window.removeEventListener('mousemove', handleMouseMove)
+        if (rafRef.current) cancelAnimationFrame(rafRef.current)
+    }
     }, [])
 
     return (
-        <div className='cursor-glow' style={{left:pos.x, top: pos.y}} />
+        <div ref={glowRef} className='cursor-glow' />
     )
 }
 export default CursorGlow
